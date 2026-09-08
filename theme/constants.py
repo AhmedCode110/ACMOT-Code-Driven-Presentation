7670eed60929d4a3c7d610833a6446c86201ab8f
Ahmed Gouda <a7medgouda1@gmail.com>
2026-09-08 15:15:17 +0300

    Set v4 presentation output filename

--- theme/constants.py
+++ theme/constants.py
@@ -19,5 +19,5 @@ MARGIN_RIGHT = 58
 MARGIN_TOP = 42
 MARGIN_BOTTOM = 42
 
-OUTPUT_FILE = "output/ACMOT_Final_Paper_Realtime_v3.pptx"
+OUTPUT_FILE = "output/ACMOT_Final_Paper_Realtime_v4.pptx"
 SOURCE_REFERENCE = "reference/original_presentation.pptx"
